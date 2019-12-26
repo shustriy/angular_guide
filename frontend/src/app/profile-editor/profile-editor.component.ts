0ef9579e754296c2763d4f193a9e031d2d639ee1
andrey <a.k.kotelnikov@gmail.com>
2019-12-26 11:46:32 +0200

    Reactive form. Add a nested FormGroup

--- frontend/src/app/profile-editor/profile-editor.component.ts
+++ frontend/src/app/profile-editor/profile-editor.component.ts
@@ -10,7 +10,13 @@ export class ProfileEditorComponent implements OnInit {
 
   profileForm = new FormGroup({
     firstName: new FormControl(''),
-    lastName: new FormControl('')
+    lastName: new FormControl(''),
+    address: new FormGroup({
+      street: new FormControl(''),
+      city: new FormControl(''),
+      state: new FormControl(''),
+      zip: new FormControl('')
+    })
   });
 
   constructor() {
@@ -22,6 +28,24 @@ export class ProfileEditorComponent implements OnInit {
   public onSubmit(event) {
     console.warn(this.profileForm.value);
     console.log('event', event);
+  }
+
+  public updateProfile() {
+    this.profileForm.patchValue({
+      firstName: 'Nancy',
+      address: {
+        street: '123 Drew Street'
+      }
+    });
+  }
+
+  public updateAddress() {
+    this.profileForm.get('address').setValue({
+      street: '451 Drew Street',
+      city: 'New-York',
+      state: '',
+      zip: ''
+    });
   }
 
 }
