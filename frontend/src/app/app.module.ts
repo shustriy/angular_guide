956649611ddd25ea429801436a57e453f867de72
Andrey Kotelnikov <andrey.kotelnikov@gameloft.com>
2019-12-31 13:33:05 +0200

    Add Template-driven Forms

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -2,21 +2,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
-import { ReactiveFormsModule } from '@angular/forms';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { NameEditorComponent } from './name-editor/name-editor.component';
 import { ProfileEditorComponent } from './profile-editor/profile-editor.component';
+import { HeroFormComponent } from './hero-form/hero-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NameEditorComponent,
-    ProfileEditorComponent
+    ProfileEditorComponent,
+    HeroFormComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ frontend/src/app/hero-form/hero-form.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+import { Hero } from '../hero';
+
+@Component({
+  selector: 'app-hero-form',
+  templateUrl: './hero-form.component.html',
+  styleUrls: ['./hero-form.component.scss']
+})
+export class HeroFormComponent implements OnInit {
+
+  public powers: string[] = ['Really Smart', 'Super Flexible',
+    'Super Hot', 'Weather Changer'];
+
+  public model: Hero = new Hero(18, 'Dr IQ', this.powers[0], 'Chuck Overstreet');
+
+  public submitted: boolean = false;
+
+  public get diagnostic() {
+    return JSON.stringify(this.model);
+  }
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  onSubmit() {
+    this.submitted = true;
+  }
+
+  newHero() {
+    this.model = new Hero(42, '', '');
+  }
+}
